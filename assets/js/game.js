e4e51c16141e2c4e9a8aceebd31e9e8db9ea8132
ening <ening6649@yahoo.com>
2021-10-07 11:35:51 -0400

    fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,56 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
+// function fight() {
+//     window.alert("The fight has begun!");
+// }
 
 var playerName = window.prompt("what is your robot's name?")
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playerName,playerAttack,playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+         );
+        playerHealth = playerHealth - enemyAttack;
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has "+ playerHealth + " health remaining."
+        );
+        if (enemyHealth <=0) {
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+        } 
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        window.alert(playerName + " has chosen to skip the fight!");
+    } else {
+        window.alert("You need to choose a valid option. Try again!");
+    }
+}
+
+fight();
+
+
 // window.alert(playerName); note variable name does not need ""
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message")
+// console.log(playerName);
+// console.log("This logs a string, good for leaving yourself a message")
 // this will do math and log 20
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
+// console.log(10+10);
+// console.log("Our robot's name is " + playerName);
 // console.log("Your robot, " + playerName + ", has won!"); always leave a space between the variable and the string so they dont stack
 
 
