18516018f83464983e59d47e00006bbff0118d8d
ening <ening6649@yahoo.com>
2021-10-10 10:33:23 -0400

    bug fix save and load

--- assets/js/game.js
+++ assets/js/game.js
@@ -139,12 +139,30 @@ var startGame = function () {
 };
 
 var endGame = function() {
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    }
-    else {
-        window.alert("You've lost your robot in battle.");
-    }
+    window.alert("The game has now ended. Let's see how you did!");
+    // check localStorage for high score, if it's not there, use 0
+    // highScore = highScore || 0 is the short hand for checking and setting highscore null to 0
+    var highScore = localStorage.getItem("highscore");
+        if (highScore === null) {
+            highScore = 0;
+        }
+        // if player has more money than the high score, player has new high score!
+        if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        } 
+        else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
+
+    // if (playerInfo.health > 0) {
+    //     window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    // }
+    // else {
+    //     window.alert("You've lost your robot in battle.");
+    // }
     var playAgainConfirm = window.confirm("Would you like to play again?");
     if (playAgainConfirm) {
         startGame();
